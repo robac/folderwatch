05fa3207e1c73e88b24dcf15394c1e632dcc4def
robac <robert@poch.cz>
2018-06-05 10:44:01 +0200

    Ubuntu install file created

---
+++ install/ubuntu/install_ubuntu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while true; do
+    read -p "Do you wish to install this program? [y/n] " yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
