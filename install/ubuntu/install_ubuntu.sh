0c1511c93145c2ea600020e2ff81a9826164796c
robac <robert@poch.cz>
2018-06-05 11:12:53 +0200

    install script changes

--- install/ubuntu/install_ubuntu.sh
+++ install/ubuntu/install_ubuntu.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+base_dir="$( cd "$(dirname "$0")/../.." ; pwd -P )"
+
 while true; do
     read -p "Do you wish to install this program? [y/n] " yn
     case $yn in
-        [Yy]* ) make install; break;;
+        [Yy]* ) break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+
+sudo apt install python-yaml python-mysqldb inotify-tools
+sudo mkdir /etc/folderwatch
+sudo ln -s "$base_dir/conf/folderwatch.conf" /etc/folderwatch
+sudo ln -s "$base_dir/bin/folderwatch" /usr/local/bin
+sudo chmod +x "$base_dir/bin/folderwatch/folderwatch.sh"
+
