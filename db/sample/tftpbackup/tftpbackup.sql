16b53db573c56f74849d8ca9675142ce4beb2bbd
robac <robert@poch.cz>
2018-06-20 13:27:04 +0200

    db change

---
+++ db/sample/tftpbackup/tftpbackup.sql
@@ -0,0 +1,22 @@
+SET NAMES utf8;
+
+CREATE DATABASE `tftpbackup`;
+
+USE `tftpbackup`;
+
+CREATE TABLE `backup` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `src_filename` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `dst_filename` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `src_directory` varchar(4096) COLLATE utf8_czech_ci NOT NULL,
+  `dst_directory` varchar(4096) COLLATE utf8_czech_ci NOT NULL,
+  `size` int(11) NOT NULL,
+  `moved` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `src_filename` (`src_filename`),
+  KEY `moved` (`moved`)
+) ENGINE=InnoDB AUTO_INCREMENT=99 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+CREATE USER 'tftpbackup'@'localhost' IDENTIFIED BY 'password'
+GRANT ALL PRIVILEGES ON tftpbackup.backup TO 'tftpbackup'@'localhost';
+
